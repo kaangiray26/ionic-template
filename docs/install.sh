4fbea0f77bc9d1f08c9289c85439bfeaad8d4af6
Kaan Giray Buzluk <11297495+kaangiray26@users.noreply.github.com>
2024-02-17 11:07:27 +0100

    Create install.sh

---
+++ docs/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget https://github.com/kaangiray26/ionic-template/archive/refs/tags/v1.zip
+
+unzip v1.zip
+
+rm v1.zip
+
+cp -a ionic-template-1/. ./
+
+rm -rf ionic-template-1.0.8
+
+rm install.sh
+
+npm i
